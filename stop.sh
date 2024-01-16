32ff203b4e1c1b2ca0b3c2b0d5798134cb7b11cc
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2024-01-16 14:29:00 +0000

    PLATUI-2751: Update stop helper script

--- stop.sh
+++ stop.sh
@@ -5,5 +5,5 @@ ARCHITECTURE=$(uname -m)
 if [ $ARCHITECTURE = "arm" ] || [ $ARCHITECTURE = "arm64" ] || [ $ARCHITECTURE = "aarch64" ]; then
   docker compose -f docker-compose.arm.yaml down
 else
-  docker compose up down
+  docker compose down
 fi
