d023c2c470f23d5648477a2f3ba11bef3ebd3757
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2024-01-31 10:26:52 +0000

    add uniq so we don't try to map ports multiple times

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export TARGET_PORTS=$(sm2 --ports | awk '{ print $1 }' | paste -sd "," -),11000,27017
+export TARGET_PORTS=$(sm2 --ports | awk '{ print $1 }' | uniq | paste -sd "," -),11000,27017
 
 ARCHITECTURE=$(uname -m)
 
