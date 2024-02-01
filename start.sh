a49772a4fdd65a870cc53ed749b72bfc696aa9f8
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2024-02-01 08:59:29 +0000

    just some wip

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ source .env
 
 TARGET_PORTS=$(sm2 --ports | awk '{ print $1 }' | uniq | grep -vxF "0" | paste -sd "," -),11000,27017,$EXTRA_PORTS
 
-DOCKER_COMPOSE_FILE=docker-compose.yaml
+DOCKER_COMPOSE_FILE=docker-compose.arm.yaml
 
 ARCHITECTURE=$(uname -m)
 if [ $ARCHITECTURE = "arm" ] || [ $ARCHITECTURE = "arm64" ] || [ $ARCHITECTURE = "aarch64" ]; then
@@ -13,8 +13,8 @@ fi
 
 # because when docker is an alias for `lima nerdctl` it doesn't pass env variables to the vm
 # docker is running within we have to bake our env variables via build arguments
-docker compose --file="$DOCKER_COMPOSE_FILE" build \
+lima nerdctl compose --file="$DOCKER_COMPOSE_FILE" build \
   --build-arg TARGET_PORTS=$TARGET_PORTS \
   --build-arg TARGET_IP=$TARGET_IP
 
-docker compose --file="$DOCKER_COMPOSE_FILE" up -d
+lima nerdctl compose --file="$DOCKER_COMPOSE_FILE" up -d
--- stop.sh
+++ stop.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-ARCHITECTURE=$(uname -m)
-
-if [ $ARCHITECTURE = "arm" ] || [ $ARCHITECTURE = "arm64" ] || [ $ARCHITECTURE = "aarch64" ]; then
-  docker compose -f docker-compose.arm.yaml down
-else
-  docker compose down
-fi
+lima nerdctl compose down --remove-orphans
