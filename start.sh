c6e08c3fb54ddfbdc9ac9c9cc15ceabe55b814f2
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2024-02-02 11:59:49 +0000

    finish refactoring from build-args to passing ports by .env

--- start.sh
+++ start.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-#if test -f .env; then
-#  source .env
-#fi
-
-TARGET_PORTS=$(sm2 --ports | awk '{ print $1 }' | uniq | grep -vxF "0" | paste -sd "," -),11000,27017,$EXTRA_PORTS
+if test -f .env; then
+  source .env
+fi
 
 DOCKER_COMPOSE="${DOCKER_COMPOSE:-docker compose}"
-DOCKER_COMPOSE_FILE=docker-compose.yaml
+
+DOCKER_COMPOSE_FILE=docker-compose.arm.yaml
 
 ARCHITECTURE=$(uname -m)
 if [ $ARCHITECTURE = "arm" ] || [ $ARCHITECTURE = "arm64" ] || [ $ARCHITECTURE = "aarch64" ]; then
   DOCKER_COMPOSE_FILE=docker-compose.arm.yaml
 fi
 
-# because when docker is an alias for `lima nerdctl` it doesn't pass env variables to the vm
-## docker is running within we have to bake our env variables via build arguments
-#$DOCKER_COMPOSE --file="$DOCKER_COMPOSE_FILE" build \
-#  --build-arg TARGET_PORTS=$TARGET_PORTS \
-#  --build-arg TARGET_IP=$TARGET_IP
+TARGET_PORTS=$(sm2 --ports | awk '{ print $1 }' | uniq | grep -vxF "0" | grep -vxF "6099" | grep -vxF "7900" | paste -sd "," -),11000,27017,$EXTRA_PORTS
 
-# originally when I tried it via env_file I used a file other than ".env" and it seems
-# like lima nerdctl compose will only grab stuff from .env - not other env_files in the
-# docker-compose.yaml config
-touch .env # in case it doesn't exist
-sed -i" " '/^TARGET_PORTS/d' .env # in case they've already been set
-echo "TARGET_PORTS=$TARGET_PORTS" >> .env # so we use the latest
-# TODO is there a better way to express the above? Seems to sometimes create multiple .env files
+# add or replace target ports in .env that should be accessible by browser on localhost
+# we have to add them to the .env file so they are available if using lima nerdctl which
+# doesn't copy environment variables from the current process into the vm running docker
+# WARNING: lima nerdctl doesn't support alternatively named env files via composes env_file
+touch .env \
+  && awk '!/^TARGET_PORTS=/' .env > temp \
+  && mv temp .env \
+  && echo "TARGET_PORTS=$TARGET_PORTS" >> .env
 
 $DOCKER_COMPOSE --file="$DOCKER_COMPOSE_FILE" up --detach --build
