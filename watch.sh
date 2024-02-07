df91b1563e58f5e12a9d48995ecf036e12daaf23
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2024-02-07 14:09:26 +0000

    tidy up watch script a bit in case we do want to include it

--- watch.sh
+++ watch.sh
@@ -8,4 +8,15 @@ fi
 
 DOCKER_COMPOSE="${DOCKER_COMPOSE:-docker compose}"
 
-$DOCKER_COMPOSE port $BROWSER 7900 | xargs -I {} open http://{}
+if [ -z "${BROWSER}" ]; then
+  echo "Provide the browser you want to watch as an argument (chrome,chromium,firefox,edge)"
+  echo "For example: ./watch.sh chrome"
+  exit 1
+fi
+
+case "$(uname)" in
+    Darwin*) OPEN="open";;
+    *)       OPEN="xdg-open"
+esac
+
+$DOCKER_COMPOSE port $BROWSER 7900 | xargs -I {} $OPEN http://{}
